b70122a218e9943d8847831ba8f0248f9a6a56c0
Miranda Galban <mirandagalbanbjj@gmail.com>
2023-09-07 00:14:25 -0300

    adding testing

--- ProductManager.js
+++ ProductManager.js
@@ -3,6 +3,20 @@ class ProductManager {
     this.products = [];
   }
 
+  //metodo getproducts
+  getProducts = () => {
+    return this.products;
+  };
+
+  //busqueda por id
+  getProductById = (id) => {
+    if (!this.products.find((product) => product.id === id)) {
+      return console.log("Not found");
+    } else {
+      console.log("Correct Id");
+    }
+  };
+
   addProduct = (title, description, price, thumbnail, code, stock) => {
     const product = {
       title,
@@ -12,24 +26,43 @@ class ProductManager {
       code,
       stock,
     };
+    //validacion de code
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i].code === code) {
+        console.log(`This code ${code} already exist`);
+        break;
+      }
+    }
     // creacion de id
     if (this.products.length == 0) {
       product.id = 1;
     } else {
-      product.id = this.products[this.product.length - 1].id + 1;
+      product.id = this.products.id + 1;
     }
     this.products.push(product);
   };
-  //busqueda por id
-  getProductById = (id) => {
-    if (!this.products.find((product) => product.id === id)) {
-      return console.log("Not found");
-    } else {
-      console.log("Correct Id");
-    }
-  };
-  //metodo getproducts
-  getProducts = () => {
-    return this.products;
-  };
 }
+
+//testing
+const manejadorProductos = new ProductManager();
+manejadorProductos.getProducts();
+console.log(manejadorProductos.products);
+manejadorProductos.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
+);
+console.log(manejadorProductos.getProducts());
+manejadorProductos.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
+);
+manejadorProductos.getProductById(1);
+manejadorProductos.getProductById(4);
