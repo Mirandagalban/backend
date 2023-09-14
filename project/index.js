c512e909ff8618c0c292739e08d72956e9612d53
Miranda Galban <mirandagalbanbjj@gmail.com>
2023-09-14 09:27:49 -0300

    testing 1

--- project/index.js
+++ project/index.js
@@ -5,6 +5,25 @@ const manager = new ProductManager("./files/products.json");
 const env = async () => {
   const productsEnv = await manager.getProducts();
   console.log(productsEnv);
+
+  const productoTest = {
+    title: "producto prueba",
+    description: "Este es un producto prueba",
+    price: 200,
+    thumbnail: "sin imagen",
+    code: "abc123",
+    stock: 25,
+  };
+
+  await manager.addProduct(productoTest);
+
+  const productsResult = await manager.getProducts();
+  console.log(productsResult);
 };
 
 env();
+const manejadorProductos = new ProductManager();
+manejadorProductos.getProducts();
+console.log(manejadorProductos.products);
+manejadorProductos.addProduct();
+console.log(manejadorProductos.getProducts());
--- project/managers/ProductManager.js
+++ project/managers/ProductManager.js
@@ -30,7 +30,7 @@ class ProductManager {
     }
   };
 
-  addProduct = (title, description, price, thumbnail, code, stock) => {
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
     const product = {
       title,
       description,
@@ -39,20 +39,41 @@ class ProductManager {
       code,
       stock,
     };
-    //validacion de code
-    for (let i = 0; i < this.products.length; i++) {
-      if (this.products[i].code === code) {
-        console.log(`This code ${code} already exist`);
-        break;
+    try {
+      const products = await this.getProducts();
+      // creacion de id
+      if (this.products.length == 0) {
+        product.id = 1;
+      } else {
+        product.id = this.products.id + 1;
       }
+      //validacion de code
+      for (let i = 0; i < this.products.length; i++) {
+        if (this.products[i].code === code) {
+          console.log(`This code ${code} already exist`);
+          break;
+        }
+      }
+      //insertamos el elemento
+      products.push(product);
+      //insertamos el file
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(products, null, "\t")
+      );
+    } catch (error) {
+      console.log(error);
     }
-    // creacion de id
-    if (this.products.length == 0) {
-      product.id = 1;
-    } else {
-      product.id = this.products.id + 1;
+  };
+
+  deleteProduct = async (product) => {
+    try {
+      if (id === product.id) {
+        await fs.promises.unlink(product);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    this.products.push(product);
   };
 }
 /*
@@ -78,7 +99,21 @@ manejadorProductos.addProduct(
   25
 );
 manejadorProductos.getProductById(1);
-manejadorProductos.getProductById(4);*/
+manejadorProductos.getProductById(4);
+
+//testing pre entrega 2
+const manejadorProductos = new ProductManager();
+manejadorProductos.getProducts();
+console.log(manejadorProductos.products);
+manejadorProductos.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
+);
+console.log(manejadorProductos.getProducts());*/
 
 module.exports = {
   ProductManager,
